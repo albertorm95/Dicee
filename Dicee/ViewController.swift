467c7cfd3f8c5c95973656672606e1f3794a38b4
Alberto <amrojas@mx1.ibm.com>
2017-10-13 19:00:50 -0500

    Dice now change to a random Dice when you click the button.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet var diceImageView1: UIImageView!
+    @IBOutlet var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        changeDiceValue(randomValue1: randomDiceIndex(), randomValue2: randomDiceIndex())
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        changeDiceValue(randomValue1: randomDiceIndex(), randomValue2: randomDiceIndex())
+    }
+    
+    func randomDiceIndex() -> Int {
+        return Int(arc4random_uniform(6))
+    }
+    
+    func changeDiceValue(randomValue1 : Int, randomValue2 : Int) {
+        diceImageView1.image = UIImage(named: diceArray[randomValue1])
+        diceImageView2.image = UIImage(named: diceArray[randomValue2])
+    }
+    
 }
 
